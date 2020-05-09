f79cbe41270eae73cc904eceaa2089e1c53e013d
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-05-09 15:20:07 -0400

    search and display meals from API

--- script.js
+++ script.js
@@ -7,38 +7,40 @@ const single_mealEl = document.getElementById('single-meal');
 
 
 // Search Meal and fetch from API
-function searchMeal(e){
+function searchMeal(e) {
     e.preventDefault();
     // clear single meal
-    single_mealEl.innerHTML = ' '
+    single_mealEl.innerHTML = ''
 
     // get search term
-    const term = search.value 
+    const term = search.value
 
     // check for empty
-    if(term.trim()){
+    if (term.trim()) {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
             .then(res => res.json())
             .then(data => {
                 console.log(data)
                 resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
 
-                if(data.meals === null){
+                if (data.meals === null) {
                     resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
-                } else{
-                    mealsEl.innerHTML = data.meals.map(meal=> `
+                } else {
+                    mealsEl.innerHTML = data.meals.map(meal => `
                         <div class="meal">
-                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-                            <div class="meal-info" data-mealID="${meal.idMeal}></div>
-                            <h3> ${meal.strMeal}</h3>
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
                         </div>
-                    `)
+                        `
+                    )
                     .join('')
                 }
             })
-            // clear search text
-            search.value = ''
-    } else{
+        // clear search text
+        search.value = ''
+    } else {
         alert('Please enter a search term')
     }
 }
