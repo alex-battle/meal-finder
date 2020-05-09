576928a098aa9b12402c21c01793227f18db9eed
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-05-09 13:46:00 -0400

    images rendering on page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const random = document.getElementById('random');
-const mealsEl = document.getElementById('mealsEl');
+const mealsEl = document.getElementById('meals');
 const resultHeading = document.getElementById('result-heading');
 const single_mealEl = document.getElementById('single-meal');
 
@@ -10,7 +10,7 @@ const single_mealEl = document.getElementById('single-meal');
 function searchMeal(e){
     e.preventDefault();
     // clear single meal
-    // single_mealEl.innerHTML = ' '
+    single_mealEl.innerHTML = ' '
 
     // get search term
     const term = search.value 
@@ -21,7 +21,23 @@ function searchMeal(e){
             .then(res => res.json())
             .then(data => {
                 console.log(data)
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
+
+                if(data.meals === null){
+                    resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
+                } else{
+                    mealsEl.innerHTML = data.meals.map(meal=> `
+                        <div class="meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+                            <div class="meal-info" data-mealID="${meal.idMeal}></div>
+                            <h3> ${meal.strMeal}</h3>
+                        </div>
+                    `)
+                    .join('')
+                }
             })
+            // clear search text
+            search.value = ''
     } else{
         alert('Please enter a search term')
     }
